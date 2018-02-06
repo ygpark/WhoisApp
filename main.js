c8a95d2d3caa2d6b39c8e32a0d8f0e2d82fa9989
Young-Gi Park <ghostyak@gmail.com>
2018-02-06 22:45:22 +0900

    배포 패키지 만들어보았음 node_modules는 resources 폴더에, 다른 파일들은 app 폴더에 넣으면 되고, 이도 저도 아니면 asar pack 폴더명 app.asar 명령으로 패키징 해서 넣으면 됨

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
   
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -54,3 +54,18 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const ipc = require('electron').ipcMain
+const dialog = require('electron').dialog
+
+ipc.on('save-dialog', function (event) {
+  const options = {
+    title: 'Save',
+    filters: [
+      { name: 'CSV(Comma Seperated Value)', extensions: ['csv'] }
+    ]
+  }
+  dialog.showSaveDialog(options, function (filename) {
+    event.sender.send('saved-file', filename)
+  })
+})
